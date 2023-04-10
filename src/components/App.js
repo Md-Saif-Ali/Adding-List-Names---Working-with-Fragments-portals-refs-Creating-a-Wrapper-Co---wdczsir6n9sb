8fb99e9f8116f913d5d39e7f46f5664eec0f162d
Md-Saif-Ali <119032699+Md-Saif-Ali@users.noreply.github.com>
2023-04-10 15:47:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,15 @@ import List from "./List";
 const App = () => {
   //code here
   return (
+   const [name, setName]=useState("")
+  const [names, setNames]=useState([])
+  return (
     <div id="main">
-       <input id="input" onChange={} />
-      <button id="button" onClick={}>Click</button>
+       <input id="input" onChange={(e)=> setName(e.target.value)} />
+      <button id="button" onClick={()=> setNames([...names, name])}>Click</button>
        <ul id="list">
-      <List listx={list} />
+        
+      <List names={names} />
       </ul>
     </div>
   )
